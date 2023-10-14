6b905bebe0ab55eeb6c0ac962a3aceaa319d0a83
GXC2356 <108477326+GXC2356@users.noreply.github.com>
2023-10-15 00:23:35 +0300

    Update aosp_vayu.mk

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -12,8 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/aosp/config/common.mk)
-$(call inherit-product, vendor/aosp/config/gsm.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 
 # Google Camera
 $(call inherit-product-if-exists, vendor/mgc/config.mk)
